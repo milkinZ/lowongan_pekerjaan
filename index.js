19bde3d68621c06a3cefed1b07b2581d5197cef0
FirnandaMichaelPutraGaniezha <michaelgeez26@gmail.com>
2022-06-11 12:13:34 +0700

    lowongan_pekerjaan

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express") // memanggil library express js
+const bodyParser = require("body-parser") // memanggil library body-
+parser
+const cors = require("cors") // memanggil library cors
+const app = express() // mengimplementasikan express
