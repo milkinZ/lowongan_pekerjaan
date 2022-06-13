04ffa86f20c25f343ef34fcadb0e3f2a6eafe52d
FirnandaMichaelPutraGaniezha <michaelgeez26@gmail.com>
2022-06-13 14:13:22 +0700

    lowongan_pekerjaan

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const bodyParser = require("body-parser") // memanggil library body-
 parser
 const cors = require("cors") // memanggil library cors
 const app = express() // mengimplementasikan express
+
+// penggunaan body-parser untuk ekstrak data request berformat JSON
+app.use(bodyParser.json())
+// penggunaan body-parser untuk ekstrak data request dari body
+app.use(bodyParser.urlencoded({extended: true}))
+// penggunaan cors agar end point dapat diakses oleh cross platform
+app.use(cors())
